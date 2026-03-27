77c3e60154c4a0fcc5df323f08f510cf2e50e83b
LarryR-cell <larrylewisrich@atomicmail.io>
2026-03-27 19:44:01 -0400

    Made the Windows setup a bit more pretty.

--- setup/windows.bat
+++ setup/windows.bat
@@ -1,9 +1,9 @@
 @echo off
 color 0a
 cd ..
-@echo on
 echo Installing haxelibs...
 echo This might take a few moments depending on your internet speed.
+pause
 haxelib install lime 8.1.2
 haxelib install openfl 9.3.3
 haxelib install flixel 5.6.1
@@ -15,6 +15,9 @@ haxelib install hxdiscord_rpc 1.2.4
 haxelib install hxvlc 2.0.1 --skip-dependencies
 haxelib set lime 8.1.2
 haxelib set openfl 9.3.3
+echo These are git libraries. You can safely type n if you have used setup before.
+echo Do NOT press n if you have not ran the ModernLegacy setup at least once.
+pause
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
